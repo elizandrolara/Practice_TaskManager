dd605c6d011d150208e95f28fc639ea7667f0a04
durel <giElostroke.89>
2022-10-12 03:39:20 -0400

    end of project, nice work

--- app/src/main/java/com/example/practice_taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/practice_taskmanager/MainActivity.kt
@@ -3,44 +3,92 @@ package com.example.practice_taskmanager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.practice_taskmanager.ui.theme.Practice_TaskManagerTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Practice_TaskManagerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    //functions to call
-                }
-            }
+            MessageWithImage(
+                message = MessageVerified(
+                    "All tasks completed",
+                    "Nice work!"
+                )
+            )
         }
     }
 }
 
+data class MessageVerified(val workTrace:String, val workComment:String)
+
 @Composable
-fun TaskCompletedWithText(tasks:String, compliment: String){
-    Text(text = tasks)
-    Text(text = compliment)
+fun MessageWithImage(message: MessageVerified){
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(
+                id = R.drawable.ic_task_completed
+            ),
+            contentDescription = "good mark",
+            contentScale = ContentScale.Inside,
+            //modifier = Modifier.fillMaxHeight(),
+            alignment = Alignment.Center
+        )
+        MessageWithText(
+            message = MessageVerified(
+                workTrace = message.workTrace,
+                workComment = message.workComment
+            )
+        )
+    }
+
+}
+
+@Composable
+fun MessageWithText(message: MessageVerified){
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+    ) {
+        Text(
+            text = message.workTrace,
+            modifier = Modifier
+                .padding(top=24.dp, bottom = 8.dp),
+            fontSize = 26.sp
+        )
+        Text(
+            text = message.workComment,
+            modifier = Modifier,
+            fontSize = 26.sp
+        )
+    }
 }
 
 
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    Practice_TaskManagerTheme {
-        Greeting("Android")
-    }
+fun PreviewMessage() {
+    MessageWithImage(
+        message = MessageVerified(
+            "All tasks completed",
+            "Nice work!"
+        )
+    )
+
 }
